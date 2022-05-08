6eca31367da9e7cfe331b0f1b9f2a6dcd05e7922
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-05-08 23:15:40 +0900

    ** 2022.5.8 쿼리수정
    -- 쿼리 틀린부분 확인
    -- 잘못된 부분 수정

--- 오랜기간_보호한_동물(1).sql
+++ 오랜기간_보호한_동물(1).sql
@@ -9,3 +9,16 @@ from(
 where sub.rn < 4;
 
 --- 틀렸어요! 원인 파악중
+
+-- 2022.5.8 원인 찾음
+-- 위와 같이 풀 경우, rn이 datetime 순서대로 매겨지지 않음.
+-- 따라서 rn은 바깥 쿼리에서 조치해주어야 함.
+-- 아래는 정답 쿼리
+select sub.name, sub.datetime
+from(
+    select name, datetime
+    from animal_ins 
+    where animal_id not in (select animal_id from animal_outs)
+    order by datetime
+) sub
+where rownum < 4;
