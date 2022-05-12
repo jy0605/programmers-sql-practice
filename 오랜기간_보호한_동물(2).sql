ac6f6ff0cc29b77fe84bc9297088289fb4b954a3
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-05-12 23:16:21 +0900

    ** 2022.5.12
    -- rownum을 subquery 외부로
    -- ins animal_id가 null이면, outs만 존재하는 row를 가지고 오는 것이므로 조건에 부합하지 않음. outer join 아닌 inner join 수행해야함.

--- 오랜기간_보호한_동물(2).sql
+++ 오랜기간_보호한_동물(2).sql
@@ -1,13 +1,10 @@
 -- Programmers_오랜기간 보호한 동물(2)
-select outs.*, rownum rn
-from animal_ins ins right outer join animal_outs outs on ins.animal_id = outs.animal_id
-where ins.animal_id is null
-order by (outs.datetime-ins.datetime) desc;
--- 코드를 입력하세요
-/*SELECT sub.animal_id, sub.name
-from (
-    select outs.*, rownum rn from animal_ins ins right outter join animal_outs outs on ins.animal_id = outs.animal_id
-    where ins.animal_id is null
+-- https://programmers.co.kr/learn/courses/30/lessons/59411
+select animal_id, name
+from(
+    select outs.animal_id, outs.name
+    from animal_ins ins inner join animal_outs outs on ins.animal_id = outs.animal_id
+    where ins.animal_id is not null
     order by (outs.datetime-ins.datetime) desc
-) sub
-where sub.rn < 3;*/
+)
+where rownum < 3;
