12fe25c52c95904c184047be7b3afcec1f42c0cb
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-05-13 23:26:56 +0900

    ** 2022.5.13
    -- 입양_시각_구하기(1)
    -- level 2

---
+++ 입양_시각_구하기.sql
@@ -0,0 +1,8 @@
+-- programmers
+-- 입양 시각 구하기(1)
+-- https://programmers.co.kr/learn/courses/30/lessons/59412?language=oracle
+select to_char(datetime,'HH24') hour, count(*) 
+from animal_outs
+where to_char(datetime,'HH24') > 8 and to_char(datetime,'HH24') < 20
+group by to_char(datetime,'HH24')
+order by to_char(datetime,'HH24');
