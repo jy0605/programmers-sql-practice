b9437bd9fde26ea10d9b3a3a7f5dab8647bca250
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-05-13 23:29:15 +0900

    Update 입양_시각_구하기.sql
    -- to_char() func
    -- to_char(datetime, 'HH24') -> get hour from datetime(string type)
    -- to_char(datetime, 'DD-MON-YYYY HH24:MI:SS') -> get datetime as the string format(string type)

--- 입양_시각_구하기.sql
+++ 입양_시각_구하기.sql
@@ -1,6 +1,9 @@
 -- programmers
 -- 입양 시각 구하기(1)
 -- https://programmers.co.kr/learn/courses/30/lessons/59412?language=oracle
+
+-- to_char(datetime, 'format')
+
 select to_char(datetime,'HH24') hour, count(*) 
 from animal_outs
 where to_char(datetime,'HH24') > 8 and to_char(datetime,'HH24') < 20
